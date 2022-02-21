d6a123a4cebb71b73785f50346b880b410568a4d
Andrew <three.cos@gmail.com>
2022-02-21 21:07:24 +0300

    Добавлена модель Рубрика

---
+++ app/Models/Topic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Рубрика
+ */
+class Topic extends Model
+{
+    use HasFactory;
+}
